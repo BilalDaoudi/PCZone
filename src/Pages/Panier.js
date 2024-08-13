c0cbdbbeb085985a650020a4f520d8744b978e7e
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-13 15:17:30 +0100

    Update Panier.js

--- src/Pages/Panier.js
+++ src/Pages/Panier.js
@@ -5,10 +5,7 @@ import Menu from "../Components/Menu";
 import "bootstrap/dist/css/bootstrap.min.css";
 import {FaTrash } from 'react-icons/fa';
 
-
-
 export default function Panier() {
-  
   const Cart = useSelector((state) => state.Cart);
   const CodePromo = useSelector((state) => state.CodePromo);
   const [total, setTotal] = useState(
