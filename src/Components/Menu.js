4aad81ae2b2c30bcbe415776750024f1992184e6
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-08-13 15:16:29 +0100

    Update Menu.js

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -9,13 +9,11 @@ export default function Menu(props) {
   let Contact = "nav-link ";
   let Products = "nav-link ";
   let Panier = "nav-link ";
-  
   if (props.active === "Home") Home += "active";
   if (props.active === "About") About += "active";
   if (props.active === "Contact") Contact += "active";
   if (props.active === "Products") Products += "active";
   if (props.active === "Panier") Panier += "active";
-  
   return (
     <nav className="navbar navbar-expand-lg" >
       <div className="container-fluid">
