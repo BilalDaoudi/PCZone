7ed9d0414595e0b576a500d566a06edd655238b6
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-11-14 22:09:40 +0100

    Update Menu.js

--- src/Components/Menu.js
+++ src/Components/Menu.js
@@ -2,6 +2,11 @@ import React from "react";
 import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
+
+
+
+
+
 export default function Menu(props) {
   const Cart = useSelector((state) => state.Cart);
   let Home = "nav-link ";
