fbbfd50f8d28bb93dae6ec10cf7de60df0228e35
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2024-11-14 22:10:20 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import { legacy_createStore } from "redux";
+
 import { Provider } from "react-redux";
+
 import reducer from "./config/reducer";
+
 const store = legacy_createStore(reducer);
+
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
+
 root.render( <Provider store={store}>
     <App />
   </Provider>
