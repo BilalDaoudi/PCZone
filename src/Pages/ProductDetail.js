0ec9101f53c3f962a54a1fc8bb68e042b3556f37
Bilal Daoudi <105610472+BilalDaoudi@users.noreply.github.com>
2025-05-07 14:11:21 +0000

    Update ProductDetail.js

--- src/Pages/ProductDetail.js
+++ src/Pages/ProductDetail.js
@@ -4,6 +4,9 @@ import { useDispatch, useSelector } from "react-redux";
 import { Add, Update } from "../config/actions";
 import Menu from "../Components/Menu";
 
+
+
+
 export default function DetailProduct() {
   
   const { id } = useParams();
